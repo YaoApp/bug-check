1bef952a9715ec3451e51957e4ac9b9e2c034456
QJAutumn <780588304@qq.com>
2023-01-20 15:20:13 +0800

    [add]change  bug add

--- scripts/change.js
+++ scripts/change.js
@@ -3,7 +3,20 @@ function OnChange(info) {
 
   let data = { attack: 10 }; // 更新消费金额数据, 消费金额
   data[info["key"]] = info.value;
-  let setting = Process("yao.form.Setting", "cphero"); // 根据新数值生成配置信息
+  let setting = Process("yao.form.Setting", "change"); // 根据新数值生成配置信息
+
+  setting["fields"]["form"]["别名"]["edit"]["props"]["options"] = [
+    {
+      label: "change后的别名1",
+      value: "change后的别名1",
+    },
+    {
+      label: "change后的别名2222",
+      value: "change后的别名222222",
+    },
+  ];
+  console.log(setting);
+
   return {
     data: data, // 更新消费金额数据, 消费金额
     setting: setting,
