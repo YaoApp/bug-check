1d90b0b571fd8f6fd2ac08e4bb32fe9614791c0d
QJAutumn <780588304@qq.com>
2023-02-06 09:22:32 +0800

    [add]新增bug-check

---
+++ scripts/change.js
@@ -0,0 +1,12 @@
+function OnChange(info) {
+  let data = { attack: 10 }; // 更新消费金额数据, 消费金额
+  data[info["key"]] = info.value;
+  let setting = Process("yao.form.Setting", "herocp"); // 根据新数值生成配置信息
+  data.attack = 400;
+  data.difficulty = 6700;
+
+  return {
+    data: data, // 更新消费金额数据, 消费金额
+    setting: setting,
+  };
+}
